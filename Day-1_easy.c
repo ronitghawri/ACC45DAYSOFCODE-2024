43ded8af3e6007ea8ad952e1b248ae9dcdd2b8f9
ronitghawri <ronitghawri@gmail.com>
2024-09-28 15:16:15 +0530

    Add files via upload

---
+++ Day-1_easy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    
+	scanf ("%d", &x);
+	if (x >=12) {
+	printf("yes team is qualified\n");
+	} else if (x <12) {
+	printf("no team is not qualified\n");
+	}
+    return 0;
+}
