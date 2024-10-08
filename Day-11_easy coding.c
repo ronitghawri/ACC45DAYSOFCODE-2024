0dc6f7b121638845826a706a55227bb44968653a
ronitghawri <ronitghawri@gmail.com>
2024-10-08 23:30:44 +0530

    Add files via upload

---
+++ Day-11_easy coding.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+       
+            printf("%d\n",abs (x-y));
+        
+        
+        
+    }
+
+	return 0;
+}
